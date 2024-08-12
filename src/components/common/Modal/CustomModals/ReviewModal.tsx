094e813408f9dc68f1350a2d99315c8ee5528213
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-08-13 02:12:13 +0900

    refactor(GN-27): 주석 정리

--- src/components/common/Modal/CustomModals/ReviewModal.tsx
+++ src/components/common/Modal/CustomModals/ReviewModal.tsx
@@ -40,7 +40,6 @@ export default function ReviewModal({
   return (
     <div className="modal-container h-[750px] w-[480px] rounded-[24px] align-center">
       <div className="h-[686px] w-[432px]">
-        {/* 제목 바 */}
         <div className="flex h-10 w-full items-center justify-between">
           <h2 className="font-kv-bold kv-text-2xl">후기 작성</h2>
           <div className="h-7 w-7 cursor-pointer" onClick={() => onClose()}>
@@ -48,9 +47,7 @@ export default function ReviewModal({
           </div>
         </div>
 
-        {/* 나머지 컨텐츠 container */}
         <div className="mt-[41px] flex h-[605px] flex-col justify-between">
-          {/* --- 체험정보 --- */}
           <div className="flex h-[126px] w-full items-center">
             <div className="relative h-[126px] w-[126px] rounded-[12px]">
               <Image
@@ -76,10 +73,8 @@ export default function ReviewModal({
             </div>
           </div>
 
-          {/* 별점 입력 */}
           <StarRating rating={rating} onRatingChange={setRating} />
 
-          {/* 후기 작성 */}
           <textarea
             className="h-[240px] w-full rounded border border-kv-gray-79 p-4 focus:border-2 focus:border-kv-primary-blue focus:outline-none"
             rows={4}
