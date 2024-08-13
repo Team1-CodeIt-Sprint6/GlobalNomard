97d75377b3e206f3451bf6749b1789f25633e28b
wch2208 <wch2208@gmail.com>
2024-08-14 01:48:56 +0900

    feat(GN-52): 날짜에 예약 정보 없을 때 처리

--- src/components/ReservationDashboardPage/DailyReservationModal/index.tsx
+++ src/components/ReservationDashboardPage/DailyReservationModal/index.tsx
@@ -11,9 +11,13 @@ export default function DailyReservationModal({
   onClose,
 }: DailyReservationModalProps) {
   const { reservationStatus, reservationDetails } = useDailyReservationData();
+  const isEmptyData =
+    reservationStatus === undefined || reservationStatus.length === 0;
+
+  if (isEmptyData) return null;
 
   return (
-    <div className="fixed h-full w-full bg-white shadow-lg pc:absolute pc:right-[28px] pc:top-[306px] pc:h-[697px] pc:w-[429px] pc:rounded-3xl pc:border pc:border-kv-gray-400 tablet:absolute tablet:right-[28px] tablet:top-[306px] tablet:h-[697px] tablet:w-[429px] tablet:rounded-3xl tablet:border tablet:border-kv-gray-400 mobile:left-0 mobile:top-0 mobile:z-10">
+    <div className="fixed h-full w-full bg-white shadow-lg pc:absolute pc:right-[28px] pc:top-[336px] pc:h-[697px] pc:w-[429px] pc:rounded-3xl pc:border pc:border-kv-gray-400 tablet:absolute tablet:right-[28px] tablet:top-[306px] tablet:h-[697px] tablet:w-[429px] tablet:rounded-3xl tablet:border tablet:border-kv-gray-400 mobile:left-0 mobile:top-0 mobile:z-10">
       <DailyReservationModalHeader onClose={onClose} />
       {reservationStatus && (
         <>
