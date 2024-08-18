f41a19e82c8bd33123614a2bfc7045e635288010
songa <thddk0121@gmail.com>
2024-08-18 17:18:30 +0900

    feat(GN-55): 알림 승인/거절 텍스트 색상 적용 로직 추가

--- src/components/myNotificatons/Notification.tsx
+++ src/components/myNotificatons/Notification.tsx
@@ -21,6 +21,17 @@ export default function Notification({
 }: NotificationProps) {
   const timeAgo = formatTimeAgo(updatedAt);
 
+  const highlightedContent = content.split(/(승인|거절)/).map((part, index) => {
+    const textColor =
+      part === '승인' ? 'text-kv-blue' : part === '거절' ? 'text-kv-red' : '';
+
+    return (
+      <span key={index} className={textColor}>
+        {part}
+      </span>
+    );
+  });
+
   const handleDelete = async () => {
     await deleteNotification(notificationId);
     refetchNotifications();
@@ -29,7 +40,7 @@ export default function Notification({
   return (
     <div className="relative mb-2 rounded-[5px] border border-kv-gray-400 bg-white px-3 py-4">
       <span className="block h-[5px] w-[5px] rounded bg-kv-blue" />
-      <p className="mt-4 break-keep text-kv-md">{content}</p>
+      <p className={`mt-4 break-keep text-kv-md`}>{highlightedContent}</p>
       <span className="text-kv-xs text-kv-gray-600">{timeAgo}</span>
       <button
         className="absolute right-3 top-3 text-kv-gray-a1"
