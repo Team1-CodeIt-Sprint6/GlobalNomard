83e52a2e9a5f0c23eebbe43bb42d5923858f61b7
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-08-19 21:06:31 +0900

    feat(GN-139): 폼 초기데이터 로드

--- src/pages/my-activity-edit/[id].tsx
+++ src/pages/my-activity-edit/[id].tsx
@@ -1,5 +1,46 @@
-import MyActivityEditForm from '@/components/MyActivityEditPage/MyActivityEditForm';
+import { GetServerSideProps } from 'next';
 
-export default function MyActivity() {
-  return <MyActivityEditForm />;
+import MyActivityEditForm from '@/components/MyActivityEditPage/MyActivityEditForm';
+import { getActivityDetail } from '@/lib/apis/getApis';
+
+interface ActivityDetailResponse {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  subImages: { id: number; imageUrl: string }[];
+  schedules: { id: number; date: string; startTime: string; endTime: string }[];
+}
+
+interface MyActivityProps {
+  initialData: ActivityDetailResponse;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.params?.id; // Optional Chaining 사용
+  if (!id) {
+    return {
+      notFound: true,
+    };
+  }
+  try {
+    const { data } = await getActivityDetail(Number(id));
+    return {
+      props: {
+        initialData: data,
+      },
+    };
+  } catch (error) {
+    console.error('Failed to fetch activity details:', error);
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export default function MyActivityEdit({ initialData }: MyActivityProps) {
+  return <MyActivityEditForm initialData={initialData} />;
 }
