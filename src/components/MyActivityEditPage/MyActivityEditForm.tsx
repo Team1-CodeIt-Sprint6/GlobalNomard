310a46b1c9e7e79ed6356f70fb41143d62703dca
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-08-19 17:18:54 +0900

    feat(GN-139): 수정페이지 파일 추가

---
+++ src/components/MyActivityEditPage/MyActivityEditForm.tsx
@@ -0,0 +1,3 @@
+export default function MyActivityEditForm() {
+  return <>수정 페이지</>;
+}
---
+++ src/pages/my-activity-edit/[id].tsx
@@ -0,0 +1,5 @@
+import MyActivityEditForm from '@/components/MyActivityEditPage/MyActivityEditForm';
+
+export default function MyActivity() {
+  return <MyActivityEditForm />;
+}
