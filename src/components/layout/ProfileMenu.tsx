3cd1af0c0a8520d2e156ba55699bc1ae578ff9ac
songa <thddk0121@gmail.com>
2024-08-20 17:37:08 +0900

    feat(GN-134): 로그인 상태 헤더에 프로필 메뉴 추가

---
+++ src/components/layout/ProfileMenu.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+
+import { onMouseDown } from '@/components/myNotificatons/NotificationModal';
+import { useLogout } from '@/lib/utils/logout';
+
+interface ProfileMenuProps {
+  closeProfileMenu: () => void;
+}
+
+export function ProfileMenu({ closeProfileMenu }: ProfileMenuProps) {
+  const logout = useLogout();
+
+  const handleLogout = () => {
+    logout();
+    closeProfileMenu();
+  };
+
+  const handleLinkClick = () => {
+    closeProfileMenu();
+  };
+
+  return (
+    <div
+      className="absolute top-[62px] m-auto w-full max-w-[1200px]"
+      onMouseDown={onMouseDown}
+    >
+      <div className="absolute right-0 flex flex-col rounded-lg border border-kv-gray-400 bg-white">
+        <Link href="/profile" className="gnb-link" onClick={handleLinkClick}>
+          내 정보
+        </Link>
+        <Link
+          href="/my-reservations"
+          className="gnb-link"
+          onClick={handleLinkClick}
+        >
+          예약 내역
+        </Link>
+        <Link
+          href="/my-activities"
+          className="gnb-link"
+          onClick={handleLinkClick}
+        >
+          내 체험 관리
+        </Link>
+        <Link
+          href="/reservation-dashboard"
+          className="gnb-link"
+          onClick={handleLinkClick}
+        >
+          예약 현황
+        </Link>
+        <button className="gnb-link" onClick={handleLogout}>
+          로그아웃
+        </button>
+      </div>
+    </div>
+  );
+}
