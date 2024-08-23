541239694fcedfc155e7dad87482f51c2760c952
wch2208 <wch2208@gmail.com>
2024-08-23 19:41:04 +0900

    refactor(GN-164): 등록 체험 없음 표시 공통컴포넌트 적용

--- src/components/ReservationDashboardPage/NoReservationsState.tsx
+++ src/components/ReservationDashboardPage/NoReservationsState.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import EmptyState from '@/components/common/EmptyState';
 
 import { ReservationDashboardHeader } from './ReservationDashboardHeader';
 
@@ -6,16 +6,7 @@ export default function NoReservationsState() {
   return (
     <div className="reservation-dashboard-container">
       <ReservationDashboardHeader />
-      <div className="flex h-full flex-col items-center justify-center">
-        <Image
-          src="/assets/images/empty_img.png"
-          alt="빈 상태 이미지"
-          width={200}
-          height={200}
-          className="mb-4 pc:size-[240px]"
-        />
-        <p className="text-2xl font-kv-medium">아직 등록한 체험이 없어요</p>
-      </div>
+      <EmptyState message="아직 등록한 체험이 없어요" />
     </div>
   );
 }
